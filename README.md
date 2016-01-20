ee42f8fe5cc1a405efdfb1591d45acc1f6266788
Austin Minnon and Cassandra Culkins <cassy.culkins@gmail.com>
2016-01-19 16:25:44 -0800

    make page functional

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Leap Year Website_
+
+#### _This website allows the user to input any random year and receive an output detailing whether or not said year is a leap-year, January 19th, 2016_
+
+#### By _**Austin Minnon & Cassandra Culkins**_
+
+## Description
+
+_This website allows the user to input any random year and receive an output detailing whether or not said year is a leap-year. It is for informational purposes._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open in a browser_
+* _JavaScript must be enabled._
+
+## Known Bugs
+
+_Page displays output in an alert rather than showing a feedback line._
+
+## Support and contact details
+
+_If you have any questions or concerns, the authors can be contacted using the GitHub information listed below._
+_GitHub user names:_
+_Austin Minnon: whi7ee_
+_Cassandra Culkins: CCulkins_
+
+## Technologies Used
+
+_JavaScript and Bootstrap were used in this page._
+
+### License
+
+*MIT License*
+
+Copyright (c) 2016 **_Austin Minnon & Cassandra Culkins_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,46 @@
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+
+$(document).ready(function() {
+  $("form#leapForm").submit(function(event) {
+
+    var year = parseInt($("input#yearInput").val());
+
+
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      // $('#yesleapyear').show();
+      alert("Yes, the year you input is a leap year.");
+    } else {
+      // $('#noleapyear').show();
+      alert("No, the year you input is not a leap year.");
+    }
+
+// function leapYear(year){
+  // return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+
+// }
+    event.preventDefault();
+  });
+});
+
+
+
+// $(document).ready(function() {
+//   $("form#leapForm").submit(function(event) {
+//
+//     var year = parseInt($("input#yearInput").val());
+//
+//     if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//       alert(working);
+//       // $('#leapyear').show();
+//
+//     } else {
+//       // $('#noleapyear').show();
+//       alert(notworking);
+//     }
+//
+// // function leapYear(year){
+//   // return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+//
+// // }
+//     event.preventDefault();
+//   });
+// });
